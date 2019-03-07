056c602878aeddeb4f735c25049fa1191e2ff485
Abhijeet Rajput <bthebasics@gmail.com>
2019-03-06 20:15:59 -0500

    Graphframes

--- src/main/scala/com/btb/spark/structuredApi/graphdata/graphData.scala
+++ src/main/scala/com/btb/spark/structuredApi/graphdata/graphData.scala
@@ -32,9 +32,39 @@ object graphData {
       .option("timestampFormat", "MM/dd/yyyy")
       .option("mode", "DROPMALFORMED")
       .csv("file:///C:/DataForCourses/airline_delay/airlineDealySmall.csv")
+      .withColumnRenamed("ORIGIN_AIRPORT_ID","src")
+        .withColumnRenamed("DEST_AIRPORT_ID","dst")
 
     airlinedelay.printSchema()
     airlinedelay.show()
+
+    // Vertex DataFrame
+    val v = spark.createDataFrame(List(
+      ("a", "Alice", 34),
+      ("b", "Bob", 36),
+      ("c", "Charlie", 30),
+      ("d", "David", 29),
+      ("e", "Esther", 32),
+      ("f", "Fanny", 36),
+      ("g", "Gabby", 60)
+    )).toDF("id", "name", "age")
+    // Edge DataFrame
+    val e = spark.createDataFrame(List(
+      ("a", "b", "friend"),
+      ("b", "c", "follow"),
+      ("c", "b", "follow"),
+      ("f", "c", "follow"),
+      ("e", "f", "follow"),
+      ("e", "d", "friend"),
+      ("d", "a", "friend"),
+      ("a", "e", "friend")
+    )).toDF("src", "dst", "relationship")
+
+    val g = GraphFrame(v, e)
+
+    g.vertices.show()
+    g.edges.show()
+
 
   }
 
